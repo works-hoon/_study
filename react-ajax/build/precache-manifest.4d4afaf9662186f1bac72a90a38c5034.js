26ce0bce7a0ea29b9092308ed94dcdf65754166e
works-hoon <works.hoon@gmail.com>
2020-02-24 21:58:21 +0900

    빌드 코드 변경

---
+++ react-ajax/build/precache-manifest.4d4afaf9662186f1bac72a90a38c5034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1778b2f69216de949312cd24e1a28906",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5be8acfec0f9935829b1",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "33429fa7e32938eb9197",
+    "url": "./static/js/2.0a33c176.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.0a33c176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be8acfec0f9935829b1",
+    "url": "./static/js/main.8aaad175.chunk.js"
+  },
+  {
+    "revision": "c4b226140a6fa544adc5",
+    "url": "./static/js/runtime-main.bd3b4533.js"
+  }
+]);
--- react-ajax/build/service-worker.js
+++ react-ajax/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b124f6beb5785010be7395146597b3b2.js"
+  "./precache-manifest.4d4afaf9662186f1bac72a90a38c5034.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
